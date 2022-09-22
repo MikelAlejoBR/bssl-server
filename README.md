67beb4213b6516e7fdbde6ab6dbf8b61b1ecbc9d
MikelAlejoBR <mbarcina@redhat.com>
2022-09-22 16:35:34 +0200

    feature: simple web socket server
    This server allows receiving messages through the web socket for now.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # bssl-server
+
+## Running the server
+
+1. Clone the repository.
+2. `cargo run`
+
+This will create a websocket server listening on the `27745` port.
---
+++ src/main.rs
@@ -0,0 +1,73 @@
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+use tungstenite::accept;
+
+// The default server address.
+const SERVER_ADDRESS: &str = "localhost";
+// The default port on which the web socket server will be listening on.
+const SERVER_PORT: &str = "27745";
+
+fn main() {
+    let address: String = format!("{}:{}", SERVER_ADDRESS, SERVER_PORT);
+
+    let server = match TcpListener::bind(&address) {
+        Ok(server) => server,
+        Err(msg) => {
+            println!("Unable to bind to the '{}' address and port '{}': {}", SERVER_ADDRESS, SERVER_PORT, msg);
+            std::process::exit(1);
+        }
+    };
+
+    println!("Server started at: {}", address);
+
+    for stream in server.incoming() {
+        let stream = match stream {
+            Ok(stream) => stream,
+            Err(msg) => {
+                println!("Unable to handle the incoming TCP stream: {}", msg);
+                continue
+            }
+        };
+
+        thread::spawn(move || handle_client(stream));
+    }
+}
+
+// Handles the incoming connection. It tries to grab the peer address and listens to incoming
+// messages. On any error the function returns and closes the connection.
+fn handle_client(stream: TcpStream) {
+    let peer_addr = match stream.peer_addr() {
+        Ok(pa) => pa,
+        Err(msg) => {
+            println!("Unable to retrieve the peer address: {}", msg);
+            return;
+        }
+    };
+
+    let mut websocket = match accept(stream) {
+        Ok(ws) => ws,
+        Err(msg) => {
+            println!("Unable to accept connection: {}", msg);
+            return;
+        }
+    };
+
+    println!("Connected peer: {}", peer_addr);
+
+    loop {
+        let msg = match websocket.read_message() {
+            Ok(msg) => msg,
+            Err(err) => {
+                match err {
+                    tungstenite::error::Error::ConnectionClosed => break,
+                    other => {
+                        println!("Error when receiving the message: {:?}", other);
+                        break;
+                    }
+                }
+            }
+        };
+
+        println!("Received message: {}", msg);
+    }
+}
